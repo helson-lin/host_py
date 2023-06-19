3c2058d9af13b098f6e38b82069e1a05b04dc6c5
root <root@v4zcipgqiifgh9n6.novalocal>
2023-06-19 16:41:16 +0800

    feat: init project

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # host_py
-auto refresh dns for github 
+auto refresh dns for github
+
+## Usage
+
+if you have node env, and have install pm2.
+you can run "pm2 start" to start the serve 
--- host.py
+++ host.py
@@ -1,16 +1,16 @@
 import os
 import requests
-#import schedule
-import time
+import schedule
 
 url = 'https://cdn.jsdelivr.net/gh/521xueweihan/GitHub520@main/hosts'
 hosts_path = '/etc/hosts' if os.name != 'nt' else r'C:\Windows\System32\drivers\etc\hosts'
+
 
 def clear():
     with open('/etc/hosts') as f:
         lines = f.readlines()
 
-    in_content = False 
+    in_content = False
     with open('/etc/hosts', 'w') as f:
         for line in lines:
             if line.startswith('# GitHub520 Host Start'):
@@ -22,21 +22,17 @@ def clear():
                     f.write(line)
 
 
-
 def download_hosts():
-    print('refresh hosts')
+    print('Refreshing hosts...')
     clear()
     resp = requests.get(url)
     with open(hosts_path, 'a') as f:
         f.write(resp.text)
-        print('write success')
+    print('Hosts file updated successfully!')
 
-#schedule.every(10).minutes.do(download_hosts)
 
-while True:  
-    print('__________')
-    download_hosts()
-    #schedule.run_pending()
-    time.sleep(60) 
+schedule.every(10).minutes.do(download_hosts)
 
+while True:
+    schedule.run_pending()
 
